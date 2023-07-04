b46dc28df033ee86dcf4e572f18f1f23509a7311
SimoneVilardo <simovila95@gmail.com>
2023-07-04 13:56:17 +0200

    esercizio base terminato

--- index.php
+++ index.php
@@ -1,5 +1,45 @@
 <?php
 
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +52,43 @@
     <title>PHP Hotel</title>
 </head>
     <body>
-        
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <table class="table">
+                        <thead>
+                            <tr class="text-center">
+                                <th scope="col">NAME</th>
+                                <th scope="col">DESCRIPTION</th>
+                                <th scope="col">PARKING</th>
+                                <th scope="col">VOTE</th>
+                                <th scope="col">DISTANCE TO CENTER</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach($hotels as $hotel) { ?>
+                                <tr class="text-center">
+                                    <th scope="row">
+                                        <?php echo $hotel['name']; ?>
+                                    </th>
+                                    <td>
+                                        <?php echo $hotel['description']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $hotel['parking']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $hotel['vote']; ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $hotel['distance_to_center']; ?>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
